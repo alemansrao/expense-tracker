6779834af90bd8ddb9f35164e5a89e80b61c2253
alemansrao <sharathrao99@gmail.com>
2024-10-24 20:27:53 +0530

    updated navbar component, added favicon

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,28 +1,32 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
 type Props = {}
 import Dropdown from './Dropdown'
+import { useSession } from 'next-auth/react'
 import { File, Layout, List, Menu, Monitor, PieChart, Plus, Search, Settings, TrendingUp, User } from 'react-feather'
+import Image from 'next/image'
 const Navbar = (props: Props) => {
+  const { data: session } = useSession()
+  const { user } = session || {};
   return (<>
     <div className="navbar z-50 bg-black">
       <div className="navbar-start">
-       <Dropdown/>
+        <Dropdown />
       </div>
       <div className="navbar-center">
         <a className="btn btn-ghost text-xl">ExTrack</a>
       </div>
       <div className="navbar-end">
-        {/* <button className="btn btn-ghost btn-circle">
-          <Search />
-        </button> */}
         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
           <div className="w-10 rounded-full">
             {/* <User className='w-full h-full'/> */}
-            <img
-              className="invert"
+            {user?.image ? (<Image
+              width={100}
+              height={100}
               alt="Tailwind CSS Navbar component"
-              src="https://lh3.googleusercontent.com/a/ACg8ocLtdJAs30aqynveHLEQBLSjp2qsyl4TiCWuCNGW-K6IjcLGVmnwaA=s96-c" />
+              src={user?.image} />)
+              : (<User className='w-full h-full' />)}
           </div>
         </div>
       </div>
